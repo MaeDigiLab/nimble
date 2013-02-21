6fbec64ddd9f52ae511ca0b8091c0c276518704a
Chris Lihosit <clihosit@gmail.com>
2013-02-21 12:04:12 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nimble::Application.config.secret_token = 'f29a0395b3b0d350fc72736cdaa351b0db12553ee233d9494ca39ed5471da07a90f609714547928c745e00f0eefa7ca7196e87f8e3988383e819e7debf16d33a'
